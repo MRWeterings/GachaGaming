4196298c47e4cb0f0ab9e85cfc5fe50ca58aceed
1115487 <s1115487@student.hsleiden.nl>
2020-06-24 17:15:56 +0200

    Generic layout main page

--- app/src/main/java/com/weterings/ikpmd/MainActivity.java
+++ app/src/main/java/com/weterings/ikpmd/MainActivity.java
@@ -3,48 +3,163 @@ package com.weterings.ikpmd;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toolbar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.weterings.ikpmd.database.LocalDbHelper;
+import com.weterings.ikpmd.database.LocalDbValues;
 
 public class MainActivity extends AppCompatActivity {
+    private double shekels;
+    private double score;
+    private int shekelMultiplier1;
+    private int shekelMultiplier2;
+    private int shekelMultiplier3;
+    private int shekelMultiplier4;
+    private boolean run = true;
+    Activity localActivity;
+    TextView ScoreView;
+    TextView ShekelsView;
+    TextView ShekelClickView;
+    TextView ShekelSecondView;
+    private LocalDbHelper localDbHelper = null;
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Eat shit");
+        localActivity = this;
+        try{
+            localDbHelper.getDbHelper(this);
+            Cursor cursor1 = localDbHelper.query(LocalDbValues.TotalScoreTables.SCORETABLE, new String[]{"*"});
+            Cursor cursor2 = localDbHelper.query(LocalDbValues.ScoreMultiplierTable.MULTIPLIERTABLE, new String[]{"*"});
+            if(!(cursor1.getCount() > 0)) {
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(LocalDbValues.TotalScoreColumn.SHEKELS, 0);
+                contentValues.put(LocalDbValues.TotalScoreColumn.SCORE, 0);
+                localDbHelper.insertIntoTable(LocalDbValues.TotalScoreTables.SCORETABLE, null, contentValues);
+                cursor1 =localDbHelper.query(LocalDbValues.TotalScoreTables.SCORETABLE, new String[]{"*"});
+            }
+            if(!(cursor2.getCount() > 0)) {
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(LocalDbValues.ScoreMultiplierColumn.MULTIPLIER1, 0);
+                contentValues.put(LocalDbValues.ScoreMultiplierColumn.MULTIPLIER2, 0);
+                contentValues.put(LocalDbValues.ScoreMultiplierColumn.MULTIPLIER3, 0);
+                contentValues.put(LocalDbValues.ScoreMultiplierColumn.MULTIPLIER4, 0);
 
-
-//
-//        Button summonbutton = (Button) findViewById(R.id.summonButton);
-//        summonbutton.setOnClickListener( new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v){
-//                Log.d("Gacha time", "Start SummonActivity");
-//                startActivity(new Intent(MainActivity.this, SummonActivity.class));
-//            }
-//        });
-//        Button collectionbutton = (Button) findViewById(R.id.collectionButton);
-//        collectionbutton.setOnClickListener( new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v){
-//                Log.d("Gacha time", "Start SummonActivity");
-//                startActivity(new Intent(MainActivity.this, Collection.class));
-//            }
-//        });
+                localDbHelper.insertIntoTable(LocalDbValues.ScoreMultiplierTable.MULTIPLIERTABLE, null, contentValues);
+                cursor2 =localDbHelper.query(LocalDbValues.ScoreMultiplierTable.MULTIPLIERTABLE, new String[]{"*"});
+            }
+            if (cursor1.getCount() > 0 || cursor2.getCount() > 0) {
+                cursor1.moveToFirst();
+                score = Double.valueOf(cursor1.getString(cursor1.getColumnIndex("score")));
+                ScoreView = findViewById(R.id.score);
+                ScoreView.setText("Score: " + score);
+                ShekelsView = findViewById(R.id.shekel);
+                ShekelClickView = findViewById(R.id.shekelClick);
+                ShekelSecondView = findViewById(R.id.shekelSecond);
+                Runnable runnable1 = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            while (run = true) {
+                                try {
+                                    Cursor cursor1 = localDbHelper.query(LocalDbValues.TotalScoreTables.SCORETABLE, new String[]{"*"});
+                                    Cursor cursor2 = localDbHelper.query(LocalDbValues.ScoreMultiplierTable.MULTIPLIERTABLE, new String[]{"*"});
+                                    cursor1.moveToFirst();
+                                    cursor2.moveToFirst();
+                                    score = Double.valueOf(cursor1.getString(cursor1.getColumnIndex("score")));
+                                    shekels = Double.valueOf(cursor1.getString(cursor1.getColumnIndex("gold")));
+                                    shekelMultiplier1 = Integer.valueOf(cursor2.getString(cursor2.getColumnIndex("modifier1")));
+                                    shekelMultiplier2 = Integer.valueOf(cursor2.getString(cursor2.getColumnIndex("modifier2")));
+                                    shekelMultiplier3 = Integer.valueOf(cursor2.getString(cursor2.getColumnIndex("modifier3")));
+                                    shekelMultiplier4 = Integer.valueOf(cursor2.getString(cursor2.getColumnIndex("modifier4")));
+                                    localActivity.runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            ScoreView.setText("Score: " + (double) Math.round(score * 100) / 100);
+                                            ShekelsView.setText("Gold: " + (double) Math.round(shekels * 100) / 100);
+                                            ShekelSecondView.setText("Shekels per Second: " + (double) Math.round((shekelMultiplier1 * .05) * (1 + shekelMultiplier2 * .15) * (1 + shekelMultiplier3 * .2) * (1 + shekelMultiplier4 * .3) * 100) / 100);
+                                            ShekelClickView.setText("Shekels per Click: " + (double) Math.round((1 + shekelMultiplier1 * .05) * (1 + shekelMultiplier2 * .15) * (1 + shekelMultiplier3 * .2) * (1 + shekelMultiplier4 * .3) * 100) / 100);
+                                        }
+                                    });
+                                    Thread.sleep(100);
+                                } catch (Exception e) {
+                                    String txt = "Main regel 94";
+                                    int duration = Toast.LENGTH_SHORT;
+                                    Toast toast = Toast.makeText(getApplicationContext(), txt, duration);
+                                    toast.show();
+                                }
+                            }
+                        } catch (Exception e) {
+                            String txt = "Main regel 102";
+                            int duration = Toast.LENGTH_SHORT;
+                            Toast toast = Toast.makeText(getApplicationContext(), txt, duration);
+                            toast.show();
+                        }
+                    }
+                };
+                Runnable runnable2 = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            while (run = true) {
+                                try {
+                                    double addition = .5 * (shekelMultiplier1 * .05) * (1 + shekelMultiplier2 * .15) * (1 + shekelMultiplier3 * .2) * (1 + shekelMultiplier4 * .3);
+                                    score += addition;
+                                    shekels += addition;
+                                    String selection = "_id = ?";
+                                    String[] selectionArgs = {"1"};
+                                    ContentValues values = new ContentValues();
+                                    values.put(LocalDbValues.TotalScoreColumn.SCORE, String.valueOf(score));
+                                    values.put(LocalDbValues.TotalScoreColumn.SHEKELS, String.valueOf(shekels));
+                                    try {
+                                        localDbHelper.updateTable(LocalDbValues.TotalScoreTables.SCORETABLE, values, selection, selectionArgs);
+                                    } catch (Exception e) {
+                                        String txt = "ERROR MainActivity l125";
+                                        int duration = Toast.LENGTH_SHORT;
+                                        Toast toast = Toast.makeText(getApplicationContext(), txt, duration);
+                                        toast.show();
+                                    }
+                                    Thread.sleep(500);
+                                } catch (Exception e) {
+                                    String txt = "ERROR MainActivity l132";
+                                    int duration = Toast.LENGTH_SHORT;
+                                    Toast toast = Toast.makeText(getApplicationContext(), txt, duration);
+                                    toast.show();
+                                }
+                            }
+                        } catch (Exception e) {
+                            String txt = "ERROR MainActivity l139";
+                            int duration = Toast.LENGTH_SHORT;
+                            Toast toast = Toast.makeText(getApplicationContext(), txt, duration);
+                            toast.show();
+                        }
+                    }
+                };
+                Thread thread1 = new Thread(runnable1);
+                Thread thread2 = new Thread(runnable2);
+                thread1.start();
+                thread2.start();
+            } else {
+                String txt = "ERROR MainActivity l151";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(getApplicationContext(),txt,duration);
+                toast.show();
+            }
+        } catch (Exception e) {
+            String txt = "Main Regel 158";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(getApplicationContext(),txt,duration);
+            toast.show();
+        }
     }
-
 
 }
